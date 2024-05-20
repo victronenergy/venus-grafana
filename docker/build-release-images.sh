6fec89aea24fe8eb0f6cec2dfc3ec6b293e040c5
Martin Man <mman@martinman.net>
2024-05-20 11:27:40 +0200

    fix(docker): tag release image with major.minor, closes #17

--- docker/build-release-images.sh
+++ docker/build-release-images.sh
@@ -14,10 +14,14 @@ TARGET=venus-grafana
 DOCKERFILE="docker/Dockerfile"
 BUILD_OPTS="--no-cache --pull --progress=plain"
 
-TAG="$OWNER/$TARGET:${VER}"
+# Always tag docker image with $VER
+TAG="-t $OWNER/$TARGET:$VER"
+MAJMIN=$(echo $VER | grep -Eo '[0-9]+\.[0-9]+')
+# Optionally tag docker image with major.minor if $VER matches SemVer spec
+[[ ! -z "$MAJMIN" ]] && TAG_MAJMIN="-t $OWNER/$TARGET:$MAJMIN"
 
 # Note that we are invoking `docker build` from our parent directory
 # with -f pointing towards our custom Dockerfile
 # and trailing . to specify directory to use as a build context
 # while omitting files speficied in .dockerignore from the build context
-(cd .. && docker buildx build ${BUILD_OPTS} -f ${DOCKERFILE} --platform $PLATFORMS -t ${TAG} --push .)
+(cd .. && docker buildx build ${BUILD_OPTS} -f ${DOCKERFILE} --platform $PLATFORMS ${TAG} ${TAG_MAJMIN} --push .)
