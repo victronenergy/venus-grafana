7aa4b13e2b41a75eb0cc08701244a09257768bf1
Martin Man <mman@martinman.net>
2024-11-12 14:38:46 +0100

    fix: move English text from entrypoint.sh to welcome.json

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 export G_DIR=/var/lib/grafana
 
 export GF_PATHS_PROVISIONING="/etc/grafana/provisioning"
 export GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH="/etc/grafana/provisioning/dashboards/welcome.json"
 
-if [ -n "$VIL_ADMIN_ADDRESS" ]; then
-  export WELCOME_TEXT="\n\nGo to venus-influx-loader [admin interface]($VIL_ADMIN_ADDRESS)"
+if [ -n "$VIL_PUBLIC_URL" ]; then
+  cp $GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH /tmp/welcome.json.tmpl
+  awk '{gsub("__VIL_PUBLIC_URL__", ENVIRON["VIL_PUBLIC_URL"]); print}' < /tmp/welcome.json.tmpl > $GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH
+  rm /tmp/welcome.json.tmpl
 fi
-
-cp $GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH /tmp/welcome.json.tmpl
-awk '{gsub("__WELCOME_TEXT__", ENVIRON["WELCOME_TEXT"]); print}' < /tmp/welcome.json.tmpl > $GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH
-rm /tmp/welcome.json.tmpl
 
 exec /run.sh
