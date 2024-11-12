cd645ef7fede7660cbb4e57004c89ddef571e39a
Harm te Hennepe <dhtehennepe@gmail.com>
2024-11-12 14:43:21 +0100

    feat: Display `$VIL_PUBLIC_URL` on `welcome.json` dashboard

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,10 @@ export G_DIR=/var/lib/grafana
 export GF_PATHS_PROVISIONING="/etc/grafana/provisioning"
 export GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH="/etc/grafana/provisioning/dashboards/welcome.json"
 
+if [ -n "$VIL_PUBLIC_URL" ]; then
+  cp $GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH /tmp/welcome.json.tmpl
+  awk '{gsub("__VIL_PUBLIC_URL__", ENVIRON["VIL_PUBLIC_URL"]); print}' < /tmp/welcome.json.tmpl > $GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH
+  rm /tmp/welcome.json.tmpl
+fi
+
 exec /run.sh
